d657352c6dab172f3698dcb22275d116821cd06b
erezdamari <33053137+erezdamari@users.noreply.github.com>
2018-06-07 20:17:00 +0300

    Revert "Erez animation"

--- app/src/main/java/com/flipo/avivams/flipo/utilities/MyView.java
+++ app/src/main/java/com/flipo/avivams/flipo/utilities/MyView.java
@@ -3,24 +3,12 @@ package com.flipo.avivams.flipo.utilities;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.Point;
-import android.graphics.Rect;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.PathShape;
 import android.view.View;
-import android.view.animation.Animation;
 
 public class MyView extends View{
 
     private Path m_Object;
-    private MyPoint m_TopLeft;
-    private MyPoint m_BottomRight;
-    private int m_Width;
-    private int m_Height;
-
 
     public MyView(Context i_Context) {
         super(i_Context);
@@ -29,15 +17,14 @@ public class MyView extends View{
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Paint paint = new Paint();
-        paint.setAntiAlias(true);
-        paint.setColor(Color.BLUE);
-        paint.setStrokeWidth(2.0f);
-        canvas.drawPath(m_Object, paint);
+        canvas.setBitmap(createBitmap());
+        this.layout(0, 0, this.getLayoutParams().width, this.getLayoutParams().height);
+        this.draw(canvas);
     }
 
     private Bitmap createBitmap(){
-        Bitmap bmp = Bitmap.createBitmap(this.getWidth(), this.getHeight(), Bitmap.Config.ARGB_8888);
+        Bitmap bmp = Bitmap.createBitmap( this.getWidth(), this.getHeight(), Bitmap.Config.ARGB_8888);
+
         return bmp;
     }
 
@@ -47,37 +34,5 @@ public class MyView extends View{
 
     public Path getObject() {
         return m_Object;
-    }
-
-    public MyPoint getBottomRight() {
-        return m_BottomRight;
-    }
-
-    public MyPoint getTopLeft() {
-        return m_TopLeft;
-    }
-
-    public void setBottomRight(MyPoint i_BottomRight) {
-        this.m_BottomRight = i_BottomRight;
-    }
-
-    public void setTopLeft(MyPoint i_TopLeft) {
-        this.m_TopLeft = i_TopLeft;
-    }
-
-    public int getMyHeight() {
-        return m_Height;
-    }
-
-    public void setHeight(int i_Height) {
-        this.m_Height = i_Height;
-    }
-
-    public void setWidth(int i_Width) {
-        this.m_Width = i_Width;
-    }
-
-    public int getMyWidth() {
-        return m_Width;
     }
 }
--- app/src/main/java/com/flipo/avivams/flipo/utilities/Shape.java
+++ app/src/main/java/com/flipo/avivams/flipo/utilities/Shape.java
@@ -1,16 +1,12 @@
 package com.flipo.avivams.flipo.utilities;
 
 import android.content.Context;
-import android.graphics.Point;
-import android.graphics.Rect;
 import android.view.View;
 
 import java.util.LinkedList;
 
 public class Shape{
     private LinkedList<Stroke> m_Shape;
-    private Rect m_Rect;
-    private Point m_Position;
 
     public Shape(LinkedList<Stroke> shape){
         m_Shape = shape;
@@ -23,28 +19,5 @@ public class Shape{
 
     public LinkedList<Stroke> getShape() {
         return m_Shape;
-    }
-
-    public void CalculateShapeRect(){
-
-        for(Stroke stroke : m_Shape){
-            //Rect strokeRect = stroke.get
-        }
-    }
-
-    public void setRect(Rect i_Rect){
-        m_Rect = i_Rect;
-    }
-
-    public Rect getRect(){
-        return m_Rect;
-    }
-
-    public void setPosition(Point i_Position){
-        m_Position = i_Position;
-    }
-
-    public Point getPosition(){
-        return m_Position;
     }
 }
